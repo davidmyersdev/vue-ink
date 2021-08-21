9974625ee0a30ef6ea21117e3c537f55e68403a0
David R. Myers <davidrmyersii@gmail.com>
2021-08-21 18:14:08 -0400

    Migrate to Ink

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,36 @@
 <template>
   <div class="app">
-    <h1>vue-markdown-editor</h1>
-    <p>A Markdown editor component for Vue that renders in place without the need for a preview pane. This is the component that powers <a href="https://github.com/voraciousdev/octo">Octo</a>.</p>
-    <MarkdownEditor ref="editor" v-model="markdown" :theme="theme" :settings="{ images: { enabled: true } }" class="editor" />
     <div class="toggle">
       <label>
-        <input v-model="theme" type="radio" value="light">
-        <span>light</span>
-      </label>
-      <label>
-        <input v-model="theme" type="radio" value="dark">
+        <input v-model="appearance" type="radio" value="dark">
         <span>dark</span>
       </label>
+      <label>
+        <input v-model="appearance" type="radio" value="light">
+        <span>light</span>
+      </label>
     </div>
+    <Ink ref="editor" v-model="markdown" :appearance="appearance" :settings="{ images: { enabled: true } }" class="editor" />
   </div>
 </template>
 
 <script>
-import MarkdownEditor from '@/components/MarkdownEditor'
+import Ink from '@/components/Ink'
 
 export default {
   name: 'App',
-  components: {
-    MarkdownEditor,
-  },
+  components: { Ink },
   data() {
     return {
       /* eslint no-template-curly-in-string: "off" */
       markdown: '# Hello, World!\n\nThis editor supports [Markdown](https://en.wikipedia.org/wiki/Markdown) with dynamic syntax highlighting for code.\n\n```js\n// a snippet\nconst greet = (name) => {\n  return `Hello, ${name}!`\n}\n```',
-      theme: 'dark',
+      appearance: 'dark',
     }
   },
   watch: {
-    theme(value) {
-      document.body.classList.remove('dark', 'light')
-      document.body.classList.add(value)
+    appearance(value) {
+      document.documentElement.classList.remove('dark', 'light')
+      document.documentElement.classList.add(value)
     },
   },
   mounted() {
@@ -42,64 +38,3 @@ export default {
   },
 }
 </script>
-
-<style>
-* {
-  box-sizing: border-box;
-}
-
-html {
-  height: -webkit-fill-available;
-}
-
-body {
-  font-family: sans-serif;
-  height: 100vh;
-  height: -webkit-fill-available;
-
-  display: flex;
-  flex-direction: column;
-}
-
-a {
-  font-weight: bold;
-}
-
-.app {
-  margin: auto;
-  max-width: 70rem;
-  width: 100%;
-}
-
-.toggle {
-  position: fixed;
-  top: 1rem;
-  right: 1rem;
-}
-
-.editor {
-  border-radius: 0.25em;
-  padding: 1em;
-}
-
-.dark {
-  background-color: #111;
-  color: #aaa;
-}
-
-.dark a {
-  color: #aaa;
-}
-
-.dark .editor {
-  background-color: #050505;
-}
-
-.light a {
-  color: #000;
-}
-
-.light .editor {
-  background-color: #fafafa;
-}
-</style>
---
+++ src/components/Ink.vue
@@ -0,0 +1,86 @@
+<template>
+  <div ref="ink"></div>
+</template>
+
+<script>
+import ink from '@writewithocto/ink'
+
+export default {
+  name: 'Ink',
+  props: {
+    appearance: {
+      type: String,
+      default: 'dark',
+      validator(appearance) {
+        return ['dark', 'light'].includes(appearance)
+      },
+    },
+    attribution: {
+      type: Boolean,
+    },
+    images: {
+      type: Boolean,
+      default: false,
+    },
+    initialSelection: {
+      type: Object,
+    },
+    spellcheck: {
+      type: Boolean,
+      default: true,
+    },
+    value: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      instance: null,
+    }
+  },
+  watch: {
+    appearance(value) {
+      this.instance.reconfigure({ appearance: value })
+    },
+    attribution(value) {
+      this.instance.reconfigure({ attribution: value })
+    },
+    images(value) {
+      this.instance.reconfigure({ images: value })
+    },
+    spellcheck(value) {
+      this.instance.reconfigure({ spellcheck: value })
+    },
+    value(value) {
+      if (this.instance.doc() !== value) {
+        this.instance.update(value)
+      }
+    },
+  },
+  methods: {
+    focus() {
+      this.instance.focus()
+    },
+    select(selection) {
+      this.instance.select(selection)
+    },
+    selection() {
+      return this.instance.selection()
+    },
+  },
+  mounted() {
+    this.instance = ink(this.$refs.ink, {
+      appearance: this.appearance,
+      attribution: this.attribution,
+      doc: this.value,
+      images: this.images,
+      selection: this.initialSelection,
+      spellcheck: this.spellcheck,
+      onChange: (doc) => {
+        this.$emit('input', doc)
+      },
+    })
+  },
+}
+</script>
